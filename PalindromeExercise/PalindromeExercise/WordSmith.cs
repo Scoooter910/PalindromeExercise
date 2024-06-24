cfba5bb603055bde39ad773eb755b71231f469a6
Michele Mostoller <MMostoller@att.net>
2024-06-24 14:20:15 -0400

    Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word1,string word2)
+        {
+            if(word1 == word2)
+            {
+             return true;
+            
+            }
+            else
+            {
+            
+            return false;
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
+using PalindromeExercise;
 using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", "racecar", true)]
+        [InlineData("kayak", "kayak", true)]
+        [InlineData("mom", "mom", true)]
+        [InlineData("dog", "cat", false)]
+        [InlineData("bird", "bird", true)]
+        public void IsAPalindrome(string word1, string word2, bool expected)
         {
-
+            //Arrange
+            WordSmith instance = new WordSmith();
+            //Act
+            bool actual = instance.IsAPalindrome(word1, word2);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
